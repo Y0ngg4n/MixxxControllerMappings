e3a2fbfc53f09878874739c60a2edffa2e6b638c
Yonggan <yonggan@obco.pro>
2024-06-16 22:24:17 +0200

    Add wheel

--- Reloop-TerminalMix8-Ultimate.js
+++ Reloop-TerminalMix8-Ultimate.js
@@ -172,3 +172,50 @@ TerminalMix8.pitchSlider = function (channel, control, value, status, group) {
   // invert pitch slider (down=faster) so it matches the labels on controller
   engine.setValue(group, "rate", -script.midiPitch(control, value, status));
 };
+
+// #     #
+// #  #  # #    # ###### ###### #
+// #  #  # #    # #      #      #
+// #  #  # ###### #####  #####  #
+// #  #  # #    # #      #      #
+// #  #  # #    # #      #      #
+//  ## ##  #    # ###### ###### ######
+
+TerminalMix8.wheelTouch = function (channel, control, value, status, group) {
+  var deckNumber = script.deckFromGroup(group);
+  //if ((status & 0xF0) === 0x90) {    // If button down
+  if (value === 0x7f) {
+    // Some wheels send 0x90 on press and release, so you need to check the value
+    var alpha = 1.0 / 8;
+    var beta = alpha / 32;
+    engine.scratchEnable(deckNumber, 128, 33 + 1 / 3, alpha, beta);
+  } else {
+    // If button up
+    engine.scratchDisable(deckNumber);
+  }
+};
+
+// The wheel that actually controls the scratching
+TerminalMix8.wheelTurn = function (channel, control, value, status, group) {
+  // --- Choose only one of the following!
+  // A: For a control that centers on 0:
+  //var newValue;
+  //if (value < 64) {
+  //    newValue = value;
+  //} else {
+  //    newValue = value - 128;
+  //}
+
+  // B: For a control that centers on 0x40 (64):
+  var newValue = value - 64;
+
+  // --- End choice
+
+  // In either case, register the movement
+  var deckNumber = script.deckFromGroup(group);
+  if (engine.isScratching(deckNumber)) {
+    engine.scratchTick(deckNumber, newValue); // Scratch!
+  } else {
+    engine.setValue(group, "jog", newValue); // Pitch bend
+  }
+};
